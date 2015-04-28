778b6d1f5ad57983d58543ed5fe60fdfe728a0ea
Levent Gunay <leventgny@gmail.com>
2015-04-28 00:49:39 -0600

    refactor input stream.

--- java-vending/src/main/java/org/basesource/vending/io/impl/TerminalInputProvider.java
+++ java-vending/src/main/java/org/basesource/vending/io/impl/TerminalInputProvider.java
@@ -1,15 +1,26 @@
 package org.basesource.vending.io.impl;
 
+import org.basesource.vending.exception.MachineRuntimeException;
 import org.basesource.vending.io.InputProvider;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class TerminalInputProvider implements InputProvider<String> {
 
-    protected Scanner in = new Scanner(System.in);
+    protected BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
     @Override
-    public String get(String tag) {
-        return in.nextLine();
+    public String get(String tag)  {
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            throw new MachineRuntimeException(e, "Error: Machine IO Problem..");
+        }
+    }
+
+    public void reader(BufferedReader in) {
+        this.in = in;
     }
 }
