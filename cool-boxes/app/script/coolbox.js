04f162243d8455fcb3a76ee2b2dd615a83e97750
Levent Gunay <leventgny@gmail.com>
2015-06-19 11:16:55 -0600

    better layout and behaviors.

--- cool-boxes/app/script/coolbox.js
+++ cool-boxes/app/script/coolbox.js
@@ -3,13 +3,28 @@
     function insertNewBox() {
         var el = $(this),
         nel = el.clone();
+        push(el, nel);
+    }
 
-        nel.insertAfter(el);
+    function deleteBox(e) {
+        var el = $(this);
+        pop(el.parents('.cool-box:first'));
+        e.stopPropagation();
+    }
+
+    function push(prev, next) {
+        next.insertAfter(prev);
+    }
+
+    function pop(box) {
+        box.remove();
     }
 
     $(function() {
 
-        $('.container-one .container-two').delegate('.cool-box', 'click.insertion', insertNewBox);
+        $('.container-one .container-two').delegate('.cool-box', 'click.insertBox', insertNewBox);
+
+        $('.container-one .container-two').delegate('.cool-box .box-heading .close', 'click.deleteBox', deleteBox);
 
     });
 
