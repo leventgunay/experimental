ffd9c9cbf7bdefe67b2f57d8998abbc15a59129a
Levent Gunay <leventgny@gmail.com>
2015-06-22 00:33:28 -0600

    better ptor conf.

--- cool-boxes/app/test/lib/baseptor.js
+++ cool-boxes/app/test/lib/baseptor.js
@@ -1,11 +1,34 @@
 module.exports = (function(ptor, dr) {
     var base = {
+        uri: 'http://localhost:9090/',
+
         disableNgSync: function() {
             dr.ignoreSynchronization = true;
         },
 
         enableNgSync: function() {
             dr.ignoreSynchronization = false;
+        },
+
+        getAbsUrl: function(p) {
+            var uri = base.uri;
+            if(p && p.length) { return uri + p ; }
+            return uri;
+        },
+
+        go: function(p) {
+            var uri = base.getAbsUrl(p);
+            dr.get(uri);
+//            base.waitLoading(uri);
+        },
+
+        waitLoading: function(u) {
+            dr.driver.wait(function() {
+                return dr.driver.getCurrentUrl(function(url) {
+                    console.log(url + ' -- ' + u );
+                    return url === u;
+                });
+            });
         }
     }; return base;
 })(protractor, browser);
--- cool-boxes/app/test/ptor/_chrome.conf.js
+++ cool-boxes/app/test/ptor/_chrome.conf.js
@@ -6,17 +6,20 @@ exports.config = {
   },
 
   beforeLaunch: function() {
-    // check out webdriver status
+    // check out webdriver
     var exec = require('child_process').execSync;
     console.log(exec("./node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-manager update --chrome", { encoding: 'utf8' }));
   },
 
   onPrepare: function() {
-    global.basep = require('../lib/baseptor');
 
+    global.basep = require('../lib/baseptor');
 
     // angular not needed.
     basep.disableNgSync();
-  }
 
+    // navigate app
+    basep.go();
+
+  }
 };
